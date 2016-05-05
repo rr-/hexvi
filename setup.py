42ebfac20d2e35752766d6603551649a055747a9
rr- <rr-@sakuya.pl>
2016-05-05 11:56:13 +0200

    Setup: reformat setup.py

--- setup.py
+++ setup.py
@@ -6,22 +6,20 @@ for root, dirs, files in os.walk('hexvi/share'):
     data_files.append((root, list(os.path.join(root, file) for file in files)))
 
 setup(
-    name             = 'hexvi',
-    description      = 'A hex editor inspired by Vim.',
-    author           = 'rr-',
-    author_email     = 'rr-@sakuya.pl',
-    url              = 'https://github.com/rr-/hexvi',
-    license          = 'MIT',
+    name='hexvi',
+    description='A hex editor inspired by Vim.',
+    author='rr-',
+    author_email='rr-@sakuya.pl',
+    url='https://github.com/rr-/hexvi',
+    license='MIT',
 
-    packages         = find_packages(),
-    data_files       = data_files,
+    packages=find_packages(),
+    data_files=data_files,
 
-    entry_points     = {
-        'console_scripts': [
-            'hexvi = hexvi.__main__:main',
-        ],
+    entry_points={
+        'console_scripts': ['hexvi=hexvi.__main__:main'],
     },
 
-    install_requires = ['urwid', 'regex'],
-    keywords         = ['hex editor', 'console', 'urwid', 'desktop'],
+    install_requires=['urwid', 'regex'],
+    keywords=['hex editor', 'console', 'urwid', 'desktop'],
 )
